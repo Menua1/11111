4311a2033ab1e77b3e6d74c1c85a5c8518919e2b
Menua <menya2012avah@mail.ru>
2022-09-30 02:19:30 +0300

    hb

---
+++ happybd/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "мария")
+{
+    Console.WriteLine("ура,это Мария");
+}
+else 
+{
+    Console.Write("привет, ");
+    Console.WriteLine(username);
+}
